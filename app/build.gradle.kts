71a5c2f387211f3b6e4051eae8fc4ad1088e918c
erik <373219+erikeelde@users.noreply.github.com>
2025-12-10 00:21:24 +0100

    Add Gradle configuration for application and module setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,40 +1,18 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    id("shorty.application")
     alias(libs.plugins.kotlin.compose)
 }
 
 android {
     namespace = "se.eelde.android.shorty"
-    compileSdk {
-        version = release(36)
-    }
-
-    defaultConfig {
-        applicationId = "se.eelde.android.shorty"
-        minSdk = 24
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
